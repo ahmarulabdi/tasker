aab8436c8c11153985a906e9f93e9dbf1c22e8b4
ahmarulabdi <rndmjck@gmail.com>
2020-05-23 15:55:24 +0700

    fix bug database connection

--- app/model/db.js
+++ app/model/db.js
@@ -1,10 +1,11 @@
 'use strict'
 
+require('dotenv').config()
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    username: process.env.USERNAME,
+    user: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 })
