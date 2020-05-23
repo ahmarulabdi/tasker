9840eb13e4ca81872233df9cdd394ae6b7f44874
ahmarulabdi <rndmjck@gmail.com>
2020-05-23 14:10:06 +0700

    adding dot env value reader

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
-app = express(),
-port = process.env.PORT || 3000
+    dotenv = require('dotenv').config(),
+    app = express(),
+    port = process.env.PORT || 3000
 
 app.listen(port)
 
